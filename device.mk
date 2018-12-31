cefeff665713885f0f6f40d15339ea5d7c50bf4b
FacuM <facumo.fm@gmail.com>
2018-12-31 08:48:09 +0000

    harpia: update vendor security patch to 2018-12-05
    As stated by LineageOS whenever the manufacturer
    stops delivering vendor image updates.

--- device.mk
+++ device.mk
@@ -45,5 +45,5 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2018-06-01
+    ro.lineage.build.vendor_security_patch=2018-12-05
 
