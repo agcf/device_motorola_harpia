7b69eb257e91552fd5472d3031c7dd69f7859aa7
FacuM <facumo.fm@gmail.com>
2019-01-22 01:28:04 +0000

    COSP bringup!

--- cosp_harpia.mk
+++ cosp_harpia.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/motorola/harpia/full_harpia.mk)
 
 # Inherit some common LOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cosp/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
 
 # Boot animation
@@ -27,7 +27,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := harpia
-PRODUCT_NAME := lineage_harpia
+PRODUCT_NAME := cosp_harpia
 PRODUCT_MODEL := Moto G Play
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
